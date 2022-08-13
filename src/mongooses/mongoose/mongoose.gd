93f9a0fe5f9faae56d16b33021008f965613a662
Janakanty <adam.kowalski.pocztaemail@gmail.com>
2022-08-13 14:17:30 +0200

    0.1

---
+++ src/mongooses/mongoose/mongoose.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var active: bool = true
+
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	power_if_active()
+
+func power_if_active():
+	if active == true:
+		if Input.is_action_just_pressed("down"):
+			powerON()
+		elif Input.is_action_just_released("down"):
+			powerOFF()
+		
+func powerON():
+	$power.play("powerON")
+	
+func powerOFF():
+	$power.play_backwards()
